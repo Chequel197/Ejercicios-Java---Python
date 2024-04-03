aa02c4920ec4e90d629ac6634c0fb35f8a304c59
Chequel197 <chequel197@gmail.com>
2024-04-03 13:53:32 -0600

    Ejericios de java y Python

---
+++ app.py
@@ -0,0 +1,12 @@
+x = 10
+y = 20
+
+#suma
+print("este es el resultado de la suma de x + y", x + y)
+#resta
+print("este es el resultado de la resta de x - y", x - y)
+#division
+print("esta es el resultado de la division de x / y", x / y)
+#multiplicacion
+print("este el resultado de la multiplicacion de x * y", x * y)
+#
